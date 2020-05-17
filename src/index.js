b6a5d0017cefddb8d31ec9cd1335f8cde0097e42
LIManuel <leonmalisov@gmail.com>
2020-05-17 17:56:57 -0400

    added subcategory display

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@
 (function getCategories() {
 	fetch("http://localhost:3000/categories")
 		.then(resp => resp.json())
-		.then(function(object) {
-			for (c of object) {
+		.then(function(cArr) {
+			for (let c of cArr) {
 				let div = document.createElement("div");
 				div.id = `${c.name.toLowerCase()}-category`
 				div.className = "category"
 				div.innerText = c.name;
 
-				div.addEventListener("click", showSub)
+				div.addEventListener("click", function() {
+					showSub(event, c.subcategories)
+				})
 
 				$("div#categories").append(div)
 				// let categories = document.getElementById("categories")
 				// let div = document.createElement("div");
 				// div.innerText = c.name;
 				// categories.appendChild(div)
-				// console.log(c.name)
+				console.log(c.subcategories)
 			}
 		})
 		.catch(function(error) {
@@ -25,6 +27,29 @@
 		})
 })()
 
-function showSub(e) {
-	console.log(e.target.id)
+function showSub(e, subcArray) {
+
+	$("div#subcategories").empty();
+	for (subc of subcArray) {
+		let div = document.createElement("div");
+		div.id = `${subc.name.toLowerCase()}-subcategory`
+		div.className = "subcategory"
+		div.innerText = subc.name;
+
+		$("div#subcategories").append(div)	
+	}
+	// object
+	// let div = document.createElement("div");
+	// div.id = `${subc.name.toLowerCase()}-subcategory`
+	// div.className = "subcategory"
+	// div.innerText = subc.name;
+
+	// $("div#subcategories").append(div)
+
+	// console.log(e.target.id)
+	// console.log(object)
 }
+
+
+
+
