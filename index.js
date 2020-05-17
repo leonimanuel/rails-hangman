f34f38db3e4d2dba52fd4f2ddbae662e679a24bc
LIManuel <leonmalisov@gmail.com>
2020-05-17 15:51:34 -0400

    added functionality to getCategories

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 //On DOM CONTENT LOAD?
-
-function getCategories() {
+// console.log("yup")
+(function getCategories() {
 	fetch("http://localhost:3000/categories")
 		.then(resp => resp.json())
 		.then(function(object) {
-			console.log("success bruh")
+			for (c of object) {
+				let categories = document.getElementById("categories")
+				let div = document.createElement("div");
+				categories.appendChild(div)
+				div.innerText = c.name;
+
+				console.log(c.name)
+			}
 		})
 		.catch(function(error) {
 			alert("failed bruh")
 		})
-}
+})()
