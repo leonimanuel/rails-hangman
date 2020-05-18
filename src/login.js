3e7b1177ea81beb5017b94b8ffa54e8deaf3e982
LIManuel <leonmalisov@gmail.com>
2020-05-18 09:07:29 -0400

    added login popup

---
+++ src/login.js
@@ -0,0 +1,8 @@
+$("div#login")[0].addEventListener("click", loginPopup)
+// $("span#close-button")
+
+function loginPopup() {
+	console.log("klork")
+	$("div#login-popup").removeClass("hidden")
+
+}
