8cc057d283d2a095f569dd1e26fce1069f2d8497
LIManuel <leonmalisov@gmail.com>
2020-05-19 18:53:12 -0400

    added challenge form on create challenge button click

--- hangman-frontend/src/challenges.js
+++ hangman-frontend/src/challenges.js
@@ -14,4 +14,19 @@ function createChallengePopup() {
 	createChallengeButton.innerText = "Create Challenge";
 	$("#challenge-popup").append(createChallengeButton);
 
+	createChallengeButton.addEventListener("click", createChallengeForm)
+}
+
+function createChallengeForm() {
+	// console.log("creating challenge form")
+	$("#challenge-popup").empty();
+	$("#challenge-popup").html('\
+		<div id="challenge-form">\
+			<label for="challenge-phrase">Phrase: </label>\
+			<input type="text" id="challenge-phrase" name="phrase"> <br>\
+			<label for="challenge-hint">Hint: </label>\
+			<input type="text" id="challenge-hint" name="hint"> <br>\
+			<button id="submit-challenge-button">submit challenge</button>\
+		</div>\
+		')
 }
