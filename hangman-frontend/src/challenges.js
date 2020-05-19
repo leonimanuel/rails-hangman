050163af163b83c9927ee5585c2d182fea2257e0
LIManuel <leonmalisov@gmail.com>
2020-05-19 19:04:21 -0400

    created fetch post request for new challenge

--- hangman-frontend/src/challenges.js
+++ hangman-frontend/src/challenges.js
@@ -21,12 +21,48 @@ function createChallengeForm() {
 	// console.log("creating challenge form")
 	$("#challenge-popup").empty();
 	$("#challenge-popup").html('\
-		<div id="challenge-form">\
-			<label for="challenge-phrase">Phrase: </label>\
-			<input type="text" id="challenge-phrase" name="phrase"> <br>\
-			<label for="challenge-hint">Hint: </label>\
-			<input type="text" id="challenge-hint" name="hint"> <br>\
-			<button id="submit-challenge-button">submit challenge</button>\
+		<div id="challenge-form-container">\
+			<form id="challenge-form">\
+				<label for="challenge-recipient">Recipient: </label>\
+				<input type="text" id="challenge-recipient-input" name="recipient"> <br>\
+				<label for="challenge-phrase">Phrase: </label>\
+				<input type="text" id="challenge-phrase-input" name="phrase"> <br>\
+				<label for="challenge-hint">Hint: </label>\
+				<input type="text" id="challenge-hint-input" name="hint"> <br>\
+				<button id="submit-challenge-button">submit challenge</button>\
+			</form>\
 		</div>\
 		')
+
+	$("#submit-challenge-button").addEventListener("click", submitChallenge())
 }
+
+function submitChallenge() {
+	let configObj = {
+		method: "POST",
+		headers: {
+			"Content_Type": "application/json",
+			Accept: "application/json"
+		},
+		body: JSON.stringify({
+			recipient: $("#challenge-recipient-input").val(),
+			phrase: $("#challenge-phrase-input").val(),
+			hint: $("#challenge-hint-input").val()
+		})
+	}
+
+	fetch("http://localhost:3000/challenges", configObj)
+		.then(resp => resp.json())
+		.then(function(object) {
+			console.log(object)
+		})
+		.catch(function(error) {
+			console.log(error.message)
+		})
+}
+ 
+
+
+
+
+
