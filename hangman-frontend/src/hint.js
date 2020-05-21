6620c723b08ec9cd56d8710fce46c0ed320a252c
LIManuel <leonmalisov@gmail.com>
2020-05-21 18:01:29 -0400

    made sure hint doesn't duplicate on multiple clicks

--- hangman-frontend/src/hint.js
+++ hangman-frontend/src/hint.js
@@ -1,6 +1,6 @@
 $("#hint-header")[0].addEventListener("click", function() {
 	console.log("executing hint-reveal")
 	let hint = game.hint
-	$("#hint-box").append(hint)
+	$("#hint-box").text(hint)
 })
 
